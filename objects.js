06f452c24454d0260a9fdbaa09b118779956ca63
Sergio Perez <sergio13prez@gmail.com>
2019-07-21 15:41:51 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -27,7 +27,8 @@ function updateObjectWithObject(targetObject, updatesObject){
 
 var meals2 = { breakfast: "oatmeal", lunch: "turkey sandwich", dinner: "steak and potatoes" };
 
+meals.breakfast = ["oatmeal", 'banana'];
 
-
+console.log(meals);
 
 
