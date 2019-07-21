3aad3925e344bf989800e9309a74e00896f30719
Sergio Perez <sergio13prez@gmail.com>
2019-07-21 15:15:56 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,9 @@
+var meals = {breakfast: 'oatmeal'}
+var anothermeal = new Object({breakfast: 'oatmeal'})
+var meals1 = {
+  breakfast: 'avocado', 
+  lunch: 'avocado',
+  dinner: 'avocado'
+}
+
+console.log(meals1)
